42d2f15d672627e572730a076970e976ff8bda81
Cheng hanbing <hanbing.cheng@gmail.com>
2025-08-17 16:55:07 +0900

    init project

---
+++ src/test/java/com/example/petstore/api/common/ApiCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.petstore.api.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCommonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
